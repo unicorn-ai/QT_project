e6959bbf9c44289b9ac35202b961c1a0eefd0764
unicorn-ai <82004982+unicorn-ai@users.noreply.github.com>
2023-11-01 11:08:32 +0800

    Add files via upload

---
+++ Qwiget/AppWindow/calculatorwidget/calculatorwidget.h
@@ -0,0 +1,53 @@
+#ifndef CALCULATORWIDGET_H
+#define CALCULATORWIDGET_H
+
+#include <QDialog>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QQueue>
+#include <QChar>
+
+
+class calculatorwidget : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit calculatorwidget(QWidget *parent = nullptr);
+
+    //explicit CalculatorWidget(QWidget *parent = nullptr);
+
+
+private:
+    QPushButton *pbt1;
+    //void pbclose();
+    void initUi();
+    void initCtrl();
+
+    bool isDigitOrDot(QChar c);
+    bool isSymbol(QChar c);
+    bool isSign(QChar c);
+    bool isNumber(QString s);
+    bool isOperator(QString s);
+    bool isLeft(QString s);
+    bool isRight(QString s);
+    int priority(QString s);
+
+    QQueue<QString> split(const QString& exp);
+    bool match(const QQueue<QString>& exp);
+    bool transform(QQueue<QString>& exp, QQueue<QString>& output);
+    QString calculator(QQueue<QString>& exp);
+    QString calculator(QString l, QString op, QString r);
+    QString expression(const QString& exp);
+
+private slots:
+    void buttonClicked();
+    void pbclose();
+
+private:
+    QLineEdit   m_edit;
+    QPushButton m_buttons[20];
+
+};
+#endif // CALCULATORWIDGET_H
+
